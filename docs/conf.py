a17d366fd28a74aacc244d49c4a67e3a3e748129
VForiel <vincent.foriel@gmail.com>
2025-11-25 10:13:24 +0100

    Mise à jour de la configuration Sphinx : ajout de la gestion des dépendances, amélioration des options d'autodoc et mise à jour du thème.

--- docs/conf.py
+++ docs/conf.py
@@ -3,12 +3,19 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+import sys
+from datetime import datetime
+
+# Add project src/ to sys.path so autodoc can import project packages
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'HELIOS'
-copyright = '2025, Vincent Foriel'
 author = 'Vincent Foriel'
+copyright = f'{datetime.now().year}, {author}'
 release = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
@@ -16,6 +23,7 @@ release = '0.1.0'
 
 extensions = [
     'myst_parser',
+    'sphinx_copybutton',
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
@@ -28,22 +36,76 @@ source_suffix = {
 }
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '.venv']
 
+# -- Autodoc configuration ---------------------------------------------------
 
+autodoc_member_order = 'bysource'
+autodoc_typehints = 'signature'
+autodoc_default_options = {
+    'members': True,
+    'show-inheritance': True,
+}
 
+# Mock imports for modules that may not be available during doc build
+autodoc_mock_imports = [
+    'matplotlib',
+    'scipy',
+    'astropy',
+    'numpy',
+]
+
+# Napoleon settings: support both Google and NumPy docstring styles
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = True
+napoleon_include_private_with_doc = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+
+# -- MyST-Parser configuration -----------------------------------------------
+
+# Enable MyST extensions for richer markdown features
+myst_enable_extensions = [
+    'amsmath',
+    'colon_fence',
+    'deflist',
+    'dollarmath',
+    'fieldlist',
+    'html_admonition',
+    'html_image',
+    'linkify',
+    'replacements',
+    'smartquotes',
+    'strikethrough',
+    'substitution',
+    'tasklist',
+]
+
+# Allow fenced code blocks to act as Sphinx directives
+myst_fence_as_directive = [
+    'autoclass',
+    'automodule',
+    'autofunction',
+    'autodata',
+    'autoexception',
+    'autosummary',
+]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'breeze'
+html_title = 'HELIOS'
+html_static_path = ['_static']
 
-html_sidebars = {
-    '**': [
-        'about.html',
-        'navigation.html',
-        'relations.html',
-        'searchbox.html',
-        'donate.html',
-    ]
+html_context = {
+    "github_user": "VForiel",
+    "github_repo": "HELIOS",
+    "github_version": "main",
+    "doc_path": "docs",
+}
+
+html_theme_options = {
+    "emojis_header_nav": True,
 }
